5aa774de21180493ef50e5f79628105d27659cb5
akanuragkumar712 <akanuragkumar712@gmail.com>
2020-08-09 19:13:30 +0530

    added forms

--- src/quizapp/forms.py
+++ src/quizapp/forms.py
@@ -2,9 +2,47 @@ from django import forms
 
 
 class CreateQuizForm(forms.Form):
-
+    quizName = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                'size': '35',
+            }
+        ),
+        label = "Quiz Name", 
+        max_length = 60,
+        help_text = "Please enter you quizzes name.")
+    quizDescription = forms.CharField(
+        #widget=forms.TextInput(attrs={'size': '40'}),
+        widget=forms.Textarea(
+            attrs = {
+                'rows': '10',
+                'cols': '40', 
+                'style': 'resize: none'
+                }),
+        label = "Quiz Description", 
+        help_text = "Please decribe what this quiz is about.")
 
 class CreateAnswerForm(forms.Form):
 
+    answerText = forms.CharField(
+        widget = forms.TextInput(attrs = {'size': '40'}),
+        label = "Answer",
+        max_length = 50,
+        help_text = "Must be less than 25 characters."
+    )
+    isCorrect = forms.BooleanField(
+        widget = forms.CheckboxInput(),
+        label = "Correct Answer",    
+        required = False,
+    )
+    pointValue = forms.IntegerField(
+        initial=0,
+        label = "Answer Value",
+    )
 
 class JoinQuizForm(forms.Form):
+    sessionId = forms.CharField(
+        widget = forms.TextInput(attrs={'class': 'session-id', 'size': '10'}),
+        label = 'Enter a quiz ID to join a quiz:',
+        max_length = 6
+    )
